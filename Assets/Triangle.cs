4264e338e29f60ebe2735cff726b87dfc23f074e
Blaise Roth <blaiseroth@gmail.com>
2017-08-18 17:55:37 +0930

    Implement working triangle painting

---
+++ Assets/Triangle.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Triangle : MonoBehaviour
+{
+    bool[] colors = new bool[3];
+
+    MeshRenderer renderer;
+
+    void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        CreateMesh();
+        UpdateColor();
+    }
+
+    void CreateMesh()
+    {
+        var collider = GetComponent<PolygonCollider2D>();
+        var mesh = gameObject.AddComponent<MeshFilter>().sharedMesh = new Mesh();
+        mesh.vertices = collider.points.Select(p => (Vector3)p).ToArray();
+        mesh.triangles = new int[] { 0, 1, 2 };
+        mesh.UploadMeshData(true);
+    }
+
+    public bool this[int index]
+    {
+        get
+        {
+            return colors[index];
+        }
+        set
+        {
+            colors[index] = value;
+            UpdateColor();
+        }
+    }
+
+    void UpdateColor()
+    {
+        if (colors[0] == colors[1] && colors[1] == colors[2])
+        {
+            renderer.material.color = Color.white;
+        }
+        else if (colors[0])
+        {
+            if (colors[1])
+                renderer.material.color = Triangles.Instance.secondayColors[0];
+            else if (colors[2])
+                renderer.material.color = Triangles.Instance.secondayColors[1];
+            else
+                renderer.material.color = Triangles.Instance.primaryColors[0];
+        }
+        else if (colors[1])
+        {
+            if (colors[2])
+                renderer.material.color = Triangles.Instance.secondayColors[2];
+            else
+                renderer.material.color = Triangles.Instance.primaryColors[1];
+        }
+        else
+        {
+            renderer.material.color = Triangles.Instance.primaryColors[2];
+        }
+    }
+}
---
+++ Assets/Triangles.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+public class Triangles : MonoBehaviour, IDragHandler, IPointerDownHandler, IScrollHandler
+{
+    [SerializeField]
+    public Color[] primaryColors;
+
+    [SerializeField]
+    public Color[] secondayColors;
+
+    Triangle[] triangles;
+
+    int selectedColor;
+
+    public static Triangles Instance { get; set; }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        ClickTriangle(eventData);
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        ClickTriangle(eventData);
+    }
+
+    void IScrollHandler.OnScroll(PointerEventData eventData)
+    {
+        selectedColor = (selectedColor + (int)Mathf.Sign(eventData.scrollDelta.x) + 3) % 3;
+    }
+
+    void ClickTriangle(PointerEventData eventData)
+    {
+        var target = eventData.pointerEnter;
+        if (target == null)
+            return;
+        var triangle = target.GetComponent<Triangle>();
+        if (triangle == null)
+            return;
+
+        triangle[selectedColor] = true;
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        triangles = GetComponentsInChildren<Triangle>();
+    }
+}
